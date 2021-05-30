fca10b046333c0a7a706fed91b3ea3f90e3665a3
Cass Agius <cassanda.agius@gmail.com>
2021-05-30 11:21:02 +1000

    Add project files.

---
+++ src/Implementation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+
+namespace SunOrientationFixes
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+        }
+    }
+}
